6f8204543e1f83d3abe146ed98cfb0f1d840fdf7
VusiGae <VusiGae@gmail.com>
2023-03-16 03:05:12 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - using puts function to print a message
+ * Return: Always return 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
