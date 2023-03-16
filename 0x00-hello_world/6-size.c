51674daed2400b9e84f1137893bda970e57f2be1
VusiGae <VusiGae@gmail.com>
2023-03-16 10:42:45 -0700

    sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - The size of various types on the computer it is compiled and run on
+ * Return: Always accept 0 (Success)
+ */
+int main(void)
+{
+char a;
+int b;
+long int x;
+long long int y;
+float z;
+printf("Size of a char: %c bytes(s) \n", sizeof(a));
+printf("Size of an int: %d bytes(s) \n", sizeof(b));
+printf("Size of a long int: %ld,%li byte(s) \n", sizeof(x));
+printf("Size of a long long int: %d,%lli byte(s) \n", sizeof(y));
+printf("Size of a float: %f bytes(s) \n" sizeof(z));
+return (0);
+}
