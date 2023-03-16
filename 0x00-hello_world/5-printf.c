a455775cac635a73ab7605222652a89285b460d0
VusiGae <VusiGae@gmail.com>
2023-03-16 04:07:58 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This program prints a message using a printf function
+ * Return: Always return 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
