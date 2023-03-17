22a5027a15e2e344142195bde3c2bab693b9dd41
VusiGae <VusiGae@gmail.com>
2023-03-17 11:52:33 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - printing numbers
+ * return - always return 0 (Success)
+ */
+int main(void)
+{
+int i;
+
+for (i = '0'; i <= '9'; i++)
+{
+	putchar(i);
+}
+	putchar('\n');
+return (0);
+}
