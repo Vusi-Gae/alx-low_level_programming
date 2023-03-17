97f2c5fd4e44d2c00fa63c13fdd58657e573a065
VusiGae <VusiGae@gmail.com>
2023-03-17 09:50:32 -0700

    Alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - C program that prints the alphabet in lowercase and  uppercase
+ * Return: Always return 0 (Success)
+ */
+nt main(void)
+{
+char z = 'a';
+char Z = 'A';
+
+while (z <= 'z')
+{
+	putchar(z);
+	z++;
+}
+while (Z <= 'Z')
+{
+	putchar(Z);
+	Z++;
+}
+putchar('\n');
+return (0);
+}
