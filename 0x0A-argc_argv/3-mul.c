10bd6a2ab32ea7ec291897cd1655f82b72cfe922
Vusi-Gae <vusigae@gmail.com>
2023-04-10 07:26:42 -0700

    Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - A program that multiplies two numbers.
+ * atoi is a function that converts a string into an int
+ * @argc: argumets
+ * @argv: An array of arguments
+ *Return: Always 0 (Success) and 1 on Error
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = res = 1;
+
+	if (argc != 3)
+	{
+		printf("%d\n", "Error");
+		return (1);
+	}
+	else
+	{
+		for (i = 0; i < argc; i++)
+		{
+		res *= atoi(argv[i]);
+		}
+		printf("%d\n", res);
+	}
+	return (0);
+}
