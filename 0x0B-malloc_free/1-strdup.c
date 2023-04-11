418c884828d74464d38a2fbdfa9e2c58d0901ce7
Vusi-Gae <vusigae@gmail.com>
2023-04-11 05:23:14 -0700

    The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -39,14 +39,14 @@ char *_strdup(char *str)
 	ptr = malloc(size * sizeof(char));
 
 	i = 0;
-	while (x < size)
+	while (i < size)
 	{
 		if (ptr == NULL)
 		{
 			return (NULL);
 	}
-		ptr[x] = str[x];
-		x++;
+		ptr[i] = str[i];
+		i++;
 	}
 	return (ptr);
 }
