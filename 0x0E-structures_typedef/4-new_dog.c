daaf7b9d777da1b4b4778930a7f40c2bff97e9a9
Vusi-Gae <vusigae@gmail.com>
2023-04-17 10:21:35 -0700

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,10 +2,10 @@
 #include "dog.h"
 
 /**
- * _strlen - Finds the length of a string
- * @s: The string to measure
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
  *
- * Return: The length of the string
+ * Return: the length of the string
  */
 int _strlen(char *s)
 {
@@ -22,12 +22,13 @@ int _strlen(char *s)
 }
 
 /**
- * _strcopy - Copies a string pointed to by src, including the
- *            terminating null byte, to a buffer pointed to by dest.
- * @dest: The buffer storing the string copy.
- * @src: The source string.
+ * *_strcpy - copies the string pointed to by src
+ * including the terminating null byte (\0)
+ * to the buffer pointed to by dest
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: string to be copied
  *
- * Return: The pointer to dest.
+ * Return: the pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
