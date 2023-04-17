9c394948f537ce00881e5f51ba5cf4b5f8b80571
Vusi-Gae <vusigae@gmail.com>
2023-04-17 04:22:33 -0700

    Pi

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -3,8 +3,8 @@
 
 /**
  * File: 1-pi.h
- * Desc: A header that defines a macro named PI
- * 	 as an an abbreviation for the token 3.14159265359
+ * Desc: Header file that defines a macro named PI
+ *       as an abbreviation for the token 3.14159265359.
  */
 
 #define PI 3.14159265359
