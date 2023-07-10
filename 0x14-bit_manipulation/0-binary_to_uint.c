6d027cf6da44c1a50c1a6209210517e1beb1386f
Vusi-Gae <vusigae@gmail.com>
2023-07-09 21:57:43 -0700

    Correction of Task 1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,19 +10,16 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	{
 	unsigned int number = 0;
 	unsigned int multiplier = 1;
 	int length = 0;
 
-	if (b == NULL)
+	if (b == '\0')
 		return (0);
 
-	/* Calculate the length of the b string */
-	for (length = 0; b[length]; length++)
-		;
+	for (length = 0; b[length];)
+	       	length++;
 
-	/* Convert binary to unsigned int */
 	for (length -= 1; length >= 0; length--)
 	{
 		if (b[length] != '0' && b[length] != '1')
