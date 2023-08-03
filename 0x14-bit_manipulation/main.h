85a61127ad1234d6374cf57cb15b83ce2d66609d
vusi-gae <vusigae@gmail>
2023-08-03 10:46:15 -0700

    0x14. C - Bit manipulation

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 unsigned int binary_to_uint(const char *);
-void print_binary(unsigned long int);
+void print_binary(unsigned long int n);
 int get_bit(unsigned long int, unsigned int);
 int set_bit(unsigned long int *, unsigned int);
 int clear_bit(unsigned long int *, unsigned int);
