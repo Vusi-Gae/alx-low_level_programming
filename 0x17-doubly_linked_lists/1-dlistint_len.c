176b9916fd30dec66ad6aeafb64cc5f6f2888a3a
vusi-gae <vusigae@gmail.com>
2023-08-17 10:19:18 -0700

    Updated

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * dlistint_len - Func Counts the number of elements in a linked dlistint_t list.
+ * dlistint_len - Counts the no of elements in a linked dlistint_t list.
  * @h: The head of the dlistint_t list.
  *
  * Return: The number of elements in the dlistint_t list.
